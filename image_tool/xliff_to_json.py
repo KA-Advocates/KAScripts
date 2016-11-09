125ab0dfdede0d9380dd4680e27e53b839de4799
Igor Popov <gonadarian@gmail.com>
2016-11-09 22:31:44 +0100

    Cleaned up XLIFF to JSON converter.

--- image_tool/xliff_to_json.py
+++ image_tool/xliff_to_json.py
@@ -3,21 +3,22 @@ import codecs
 from xmljson import badgerfish as bf
 from xml.etree.ElementTree import fromstring
 
+# folder containing XLIFF files
+folder = '.\\'
+
 def xliff_to_json(filename):
-    folder = 'C:\\Users\\Igor\\Downloads\\Khan\\khan-xliff\\'
     filename = folder + filename
     with codecs.open(filename, 'r', 'utf-8') as fin:
         xliff = fin.read()
         jsonx = bf.data(fromstring(xliff))
 
     for file in jsonx['xliff']['file']:
+        # do some cleanup
         if 'group' in file['body']:
             file['body'].pop('group')
-##        if len(file['body']) == 1:
-##            if 'note' in file['body']['trans-unit']:
-##                file['body']['trans-unit'].pop('note')
-##            continue
+        # go through all strings...
         for unit in file['body']['trans-unit']:
+            # ... and remove notes to reduce file size
             if 'note' in unit:
                 try:
                     unit.pop('note')
@@ -25,12 +26,15 @@ def xliff_to_json(filename):
                     print("\tERROR: Unit is ", unit, "\n", str(e))
                     print(json.dumps(file, indent=2))
                     file['body']['trans-unit'].pop('note')
-##                    print(file)
+
+    # save new content as JSON file
     with open(filename + '.2.json', 'w', encoding='utf8') as fout:
         json.dump(jsonx, fout, ensure_ascii=False)
 
 
-##xliff_to_json('1_high_priority_platform.xliff')
-##xliff_to_json('2_high_priority_content.xliff')
-##xliff_to_json('3_medium_priority.xliff')
+# previously downloaded XLIFF files from Crowdin
+# remove xmlns="urn:oasis:names:tc:xliff:document:1.2" from files
+xliff_to_json('1_high_priority_platform.xliff')
+xliff_to_json('2_high_priority_content.xliff')
+xliff_to_json('3_medium_priority.xliff')
 xliff_to_json('4_low_priority.xliff')
