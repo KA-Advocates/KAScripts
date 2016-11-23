eb680b26d18a7897b17e819c163b18a749e34f71
Hitoshi Yamauchi <yamauchih@github.com>
2016-11-23 20:03:03 +0100

    Fixed the last output of the time line.
    Added the one before + self.__last_subtitle_str_duration
    instead of 99:99.

--- srt_tool/txt2srt/txt2srt.py
+++ srt_tool/txt2srt/txt2srt.py
@@ -19,6 +19,28 @@ class Txt2srt(object):
         self.__last_time_str = '0:00'
         self.__cur_line      = 1
         self.__last_content  = None
+        # Assume the last subtitle string has 5 seconds duration
+        self.__last_subtitle_str_duration = 5
+
+
+    def __get_second_from_time_str(self, time_str):
+        """get a second (an integer) from time_str
+        \param[in] time_str time string. E.g., '10:15'
+        \return second in integer E.g., 615 for '10:15'
+        """
+        [min, sec] = re.split(':', time_str)
+        return 60 * int(min) + int(sec)
+
+    def __get_time_str_from_second(self, sec):
+        """get a time string (e.g., '10:15') from a seconds
+        \param[in] sec integer second. E.g., 615
+        \return time string E.g., '10:15' for 615
+        """
+        min_part = int(sec / 60)
+        sec_part = sec - (min_part * 60)
+        retstr = '%02d:%02d' % (min_part, sec_part)
+        return retstr
+
 
     def __get_srt_time_str(self, time_str):
         """output srt time format from YouTube transcript time format
@@ -30,7 +52,12 @@ class Txt2srt(object):
 
     def __out_one_item(self, line_time, line_content):
         """output one item using current state
+        \param[in] line_time    current time
+        \param[in] line_content current line contents
         """
+        # DELETEME print('line_time: {0}, line_content: {1}'.format(line_time, line_content))
+
+        # For the first time. We don't know the end time yet.
         if self.__last_content == None:
             self.__last_content = line_content
             return
@@ -50,7 +77,6 @@ class Txt2srt(object):
         self.__last_content = line_content
 
 
-
     def conv(self):
         """convert the file
         """
@@ -66,8 +92,11 @@ class Txt2srt(object):
             # print(line_content)
             self.__out_one_item(line_time, line_content)
 
-        # output the last contents
-        self.__out_one_item('99:99', 'end')
+        # output the last contents. Assume
+        # self.__last_subtitle_str_duration seconds for the last subtitle string
+        last_sec = self.__get_second_from_time_str(self.__last_time_str) +\
+                   self.__last_subtitle_str_duration
+        self.__out_one_item(self.__get_time_str_from_second(last_sec), 'end')
 
 
 def main():
